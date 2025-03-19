28d9c091f0c8a0405e6d67807ef0ad6943b025db
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:33:21 +0800

    Added Files using Linux

---
+++ project/custom_banner.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/custom_banner.css">
+
+</head>
+<body>
+
+<section class="home">
+
+   <div class="content" style="width: 85%;">
+      <h1>Practicality at its Finest: A Simple, Yet Effective Web-Based Book Archiving With a Strong Emphasis on Web Design and Development</h1>
+      <p>A Capstone project of UI/UX student, in partial fulfillment of the requirements for the Associate <br>in Computer Technology Degree. </p>
+      <a href="about.php" class="white-btn">Learn more</a>
+
+   </div>
+   
+
+</section>
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
