c302cb5e01fd1d1aad2cbf1422b4a31b72b4b7fb
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:33:53 +0800

    Added Files using Linux

---
+++ project/footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="orders.php">orders</a>
+         <a href="cart.php">policies</a>
+         <a href="cart.php">cart</a>
+         <a href="login.php">logout</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p style="color: #8e44ad"><i class="fas fa-envelope"></i> [censored]</p>
+         <p> <i class="fas fa-map-marker-alt"></i> Taguig City, Philippines - 1630 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+</section>
