9813527ac63751f5a1e82896c79211a3100ae62f
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:30:45 +0800

    Added Files using Linux

---
+++ project/added_services.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS File-->
+    <link rel="stylesheet" href="css/added_services.css">
+    <title>Responsive</title>
+
+</head>
+<body>
+    
+    <!-- === Header section START ==== -->
+    
+    <header> 
+        <!--
+            <div class="logo">
+        <a href="#"><img src="img/company-logo.png" alt="company-logo"></a>
+        </div>
+        -->
+
+        <!--
+        <nav>
+            <ul id="ResMenu">
+                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#service">Services</a></li>
+                <li><a href="#portfolio">Our Work</a></li>
+                <li><a href="#team">Our Team</a></li>
+                <li><a href="#contact" class="glob-btn">Contact Us <i class="fa-solid fa-arrow-right"></i></a></li>
+            </ul>
+        </nav>  
+        -->
+        
+        <!-- Responsive Menu Button -->
+        <i class="fa-solid fa-bars" id="MenuBtn"></i>
+    </header>
+    
+    <!-- === Header section END === -->
+
+    <!-- ======================================================================================================= -->
+
+    <!-- === Feature Section START === -->
+    <section id="feature" class="padding">
+
+        <h3 class="sub-heading center">Features </h3>
+        <h2 class="heading center">Our Featured Services</h2>
+        <p class="sub-para center">Our Team is providing services with apassion while exploring the market trends.</p>
+
+        <div class="feature-row">
+
+            <!-- Feature Item 1-->
+            <div class="feature-box">
+                <i class="fa-solid fa-magnifying-glass-chart"></i>
+                <h3>Safety & Realibility</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, accusantium.</p>
+            </div>
+             <!-- Feature Item 2-->
+            <div class="feature-box">
+                 <i class="fa-regular fa-lightbulb"></i>
+                 <h3>Project Analysis</h3>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, accusantium.</p>
+                </div>
+            <!-- Feature Item 3-->
+            <div class="feature-box">
+                <i class="fa-solid fa-chart-gantt"></i>
+                <h3>One-Up Support</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, accusantium.</p>
+            </div>
+
+        </div>
+
+
+    </section>
+    <!-- Feature Section END ==== -->
+
+    <!-- ============================================================================================== -->
+
+    <!-- FontAwesome JS File (Icon File) -->
+    <script src="https://kit.fontawesome.com/45a62183ec.js" crossorigin="anonymous"></script>
+    <!-- Main JS File -->
+    <script src="js/added_services.js"></script>
+
+</body>
+</html>
