7301256846be06e1d77baffc5a8be5ff8f7027fb
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:33:37 +0800

    Added Files using Linux

---
+++ project/custom_services.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/custom_services.css">
+
+</head>
+
+<body>
+
+    <!-- Content goes here! -->
+
+    <section class="icons-conta">
+        <div class="icons">
+            <i class="fa-solid fa-download fa-4x" style="color: #fff;"></i>
+            <div class="info">
+                <h3 style="color: #fff;">Free to Access</h3>
+                <p style="color: #a6bcce">Enjoy free browsing on all contents</p>
+            </div>
+        </div>
+
+        <div class="icons">
+            <i class="fa-solid fa-palette fa-4x" style="color: #fff;"></i>
+            <div class="info">
+                <h3 style="color: #fff;">Minimalist Design</h3>
+                <p style="color: #a6bcce">Experience the beauty of simplicity</p>
+            </div>
+        </div>
+
+        <div class="icons">
+            <i class="fa-solid fa-book fa-4x" style="color: #fff;"></i>
+            <div class="info">
+                <h3 style="color: #fff;">Exclusive Contents</h3>
+                <p style="color: #a6bcce">UI/UX Designing and Web Development</p>
+            </div>
+        </div>
+
+        <div class="icons">
+            <i class="fa-solid fa-magnifying-glass fa-4x" style="color: #fff;"></i>
+            <div class="info">
+                <h3 style="color: #fff;">Easy to Navigate</h3>
+                <p style="color: #a6bcce">Simplify your reading experience</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- optional
+        <section class="home-contact">
+            
+            <div class="card-container">
+                <div class="card">
+                    <div class="face1">
+                        <div class="imgbox">
+                            <i class="fa-solid fa-truck fa-4x"></i>
+                            <h3>SHIPPING</h3>
+                        </div>
+                    </div>
+
+                    <div class="face2">
+                        <div class="content">
+                            <p>sample</p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card">
+                    <div class="face1">
+                        <div class="imgbox">
+                            <i class="fa-solid fa-award fa-4x"></i>
+                            <h3>Services</h3>
+                        </div>
+                    </div>
+
+                    <div class="face2">
+                        <div class="content">
+                            <p>sample</p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card">
+                    <div class="face1">
+                        <div class="imgbox">
+                            <i class="fa-solid fa-award fa-4x"></i>
+                            <h3>Services</h3>
+                        </div>
+                    </div>
+
+                    <div class="face2">
+                        <div class="content">
+                            <p>sample</p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card">
+                    <div class="face1">
+                        <div class="imgbox">
+                            <i class="fa-solid fa-award fa-4x"></i>
+                            <h3>Services</h3>
+                        </div>
+                    </div>
+
+                    <div class="face2">
+                        <div class="content">
+                            <p>sample</p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    
+    end here -->
+
+    <!-- optional content 
+        <div class="wrapper">
+            <h1>Our services are the best</h1>
+            <p>example here</p>
+
+            <div class="content-box">
+                
+                <a href="">
+                    <div class="card">
+                        <i class="fa-solid fa-truck fa-3x"></i>
+                        <h2>FREE SHIPPING</h2>
+                        <p>example here</p>
+                    </div>
+                </a>
+
+                <a href="">
+                    <div class="card">
+                        <i class="fa-solid fa-award fa-3x"></i>
+                        <h2>SIMPLICITY</h2>
+                        <p>example here</p>
+                    </div>
+                </a>
+
+                <a href="">
+                    <div class="card">
+                        <i class="fa-solid fa-dollar-sign fa-3x"></i>
+                        <h2>AFFORDABLE</h2>
+                        <p>example here</p>
+                    </div>
+                </a>
+
+            </div>
+
+
+        </div>
+    End here -->
+
+    <!-- End here -->
+
+
+    <!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
