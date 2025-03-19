0cb1b0dc49f4676a52c2e54d4f8e802cb10ee4a6
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:32:08 +0800

    Added Files using Linux

---
+++ project/authors.php
@@ -0,0 +1,268 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+include 'components/like_post.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>author</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="loader-container">
+   <div class="loader">
+   <div>
+      <ul>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+      </ul>
+   </div><span>Loading..</span></div>
+   <div class="blur-background"></div>
+</div>
+   
+<!-- header section starts  -->
+<header class="header">
+
+   <section class="flex">
+
+      <div class="icons">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+               $count_user_comments = $conn->prepare("SELECT * FROM `comments` WHERE user_id = ?");
+               $count_user_comments->execute([$user_id]);
+               $total_user_comments = $count_user_comments->rowCount();
+               $count_user_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ?");
+               $count_user_likes->execute([$user_id]);
+               $total_user_likes = $count_user_likes->rowCount();
+         ?>
+         <a href="user_comments.php" style="color: #a6bcce; font-size: 2.5rem; margin-right: 1.0rem;"><i class="fas fa-comment"></i><span style="margin: 0 0.40rem; color: #a6bcce;">(<?= $total_user_comments; ?>)</span></a>
+         <a href="user_likes.php" style="color: #a6bcce; font-size: 2.5rem;"><i class="fas fa-heart"></i><span style="margin: 0 0.40rem; color: #a6bcce;">(<?= $total_user_likes; ?>)</span></a>
+      </div>
+
+      <form action="search.php" method="POST" class="search-form">
+         <input type="text" name="search_box" class="box" maxlength="100" placeholder="search for books" required>
+         <button type="submit" class="fas fa-search" name="search_btn"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars" style="margin-left: 1.2rem;"></div>
+         <div id="search-btn" class="fas fa-search" style="margin-left: 1.2rem;"></div>
+         <div id="user-btn" class="fas fa-user" style="margin-left: 1.2rem;"></div>
+      </div>
+
+      <nav class="navbar">
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="posts.php"> <i class="fas fa-angle-right"></i> posts</a>
+         <a href="all_category.php"> <i class="fas fa-angle-right"></i> category</a>
+         <a href="added_blogs.php"> <i class="fas fa-angle-right"></i> blogs</a>
+         <a href="added_policies.php"> <i class="fas fa-angle-right"></i> policies</a>
+         
+      </nav>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <p class="name">Hello! <font color="#4834d4"><?= $fetch_profile['name']; ?></font></p>
+         <a href="update.php" class="btn">update profile</a>
+         <!-- THE DEVELOPER HIDE THIS FEATURES FOR MEANTIME
+            <div class="flex-btn">
+               <a href="login.php" class="option-btn">login</a>
+               <a href="register.php" class="option-btn">register</a>
+            </div> 
+         ENDS HERE! -->
+         <a href="./login.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         <?php
+            }else{
+         ?>
+            <p class="name">please login first!</p>
+            <a href="login.php" class="option-btn">login</a>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+
+</header>
+
+<div class="box-container">
+   <?php
+      }else{
+   ?>
+      <p class="name">login or register!</p>
+      <div class="flex-btn">
+         <a href="login.php" class="option-btn">login</a>
+         <a href="register.php" class="option-btn">register</a>
+      </div> 
+   <?php
+      }
+   ?>
+
+      <!-- THE DEVELOPER HIDE THIS FEATURES FOR MEANTIME
+         <div class="box">
+            <p>authors</p>
+            <div class="flex-box">
+            <?php
+               /*
+               $select_authors = $conn->prepare("SELECT DISTINCT name FROM `admin` LIMIT 10");
+               $select_authors->execute();
+               if($select_authors->rowCount() > 0){
+                  while($fetch_authors = $select_authors->fetch(PDO::FETCH_ASSOC)){ 
+               */
+            ?>
+               /*
+               <a href="author_posts.php?author= <?= $fetch_authors['name']; ?>" class="links"><?= $fetch_authors['name']; ?></a>
+               */
+               <?php
+            /*} 
+            }else{
+               echo '<p class="empty">no posts added yet!</p>';
+            } */
+            ?>  
+            <a href="authors.php" class="btn">view all</a> 
+            </div>
+         </div>
+      ENDS HERE! -->
+   
+
+</div>
+<!-- header section ends -->
+
+
+<section class="authors">
+
+   <h1 class="heading">authors</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_author = $conn->prepare("SELECT * FROM `admin`");
+      $select_author->execute();
+      if($select_author->rowCount() > 0){
+         while($fetch_authors = $select_author->fetch(PDO::FETCH_ASSOC)){ 
+
+            $count_admin_posts = $conn->prepare("SELECT * FROM `posts` WHERE admin_id = ? AND status = ?");
+            $count_admin_posts->execute([$fetch_authors['id'], 'active']);
+            $total_admin_posts = $count_admin_posts->rowCount();
+
+            $count_admin_likes = $conn->prepare("SELECT * FROM `likes` WHERE admin_id = ?");
+            $count_admin_likes->execute([$fetch_authors['id']]);
+            $total_admin_likes = $count_admin_likes->rowCount();
+
+            $count_admin_comments = $conn->prepare("SELECT * FROM `comments` WHERE admin_id = ?");
+            $count_admin_comments->execute([$fetch_authors['id']]);
+            $total_admin_comments = $count_admin_comments->rowCount();
+
+   ?>
+   <div class="box">
+      <p>author : <span><?= $fetch_authors['name']; ?></span></p>
+      <p>total posts : <span><?= $total_admin_posts; ?></span></p>
+      <p>posts likes : <span><?= $total_admin_likes; ?></span></p>
+      <p>posts comments : <span><?= $total_admin_comments; ?></span></p>
+      <a href="author_posts.php?author=<?= $fetch_authors['name']; ?>" class="btn">view posts</a>
+   </div>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no authors found</p>';
+   }
+   ?>
+
+   </div>
+      
+</section>
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+   // Select the loader and background elements
+   const loaderContainer = document.querySelector('.loader-container');
+   const blurBackground = document.querySelector('.blur-background');
+
+   // Show the loader and background on page load
+   window.addEventListener('load', () => {
+   loaderContainer.style.display = 'flex';
+   blurBackground.style.display = 'block';
+   });
+
+   // Hide the loader and background after 3 seconds
+   setTimeout(() => {
+   loaderContainer.style.display = 'none';
+   blurBackground.style.display = 'none';
+   }, 3000);
+
+</script>
+
+</body>
+</html>
