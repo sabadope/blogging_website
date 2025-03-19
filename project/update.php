2bfa0c39c38610023a8dfef16969796c682206dc
FERTONY <sabado.f.bscs@gmail.com>
2025-03-20 05:36:02 +0800

    Added Files using Linux

---
+++ project/update.php
@@ -0,0 +1,280 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:home.php');
+};
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   
+   if(!empty($name)){
+      $update_name = $conn->prepare("UPDATE `users` SET name = ? WHERE id = ?");
+      $update_name->execute([$name, $user_id]);
+   }
+
+   if(!empty($email)){
+      $select_email = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+      $select_email->execute([$email]);
+      if($select_email->rowCount() > 0){
+         $message[] = 'email already taken!';
+      }else{
+         $update_email = $conn->prepare("UPDATE `users` SET email = ? WHERE id = ?");
+         $update_email->execute([$email, $user_id]);
+      }
+   }
+
+   $empty_pass = 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
+   $select_prev_pass = $conn->prepare("SELECT password FROM `users` WHERE id = ?");
+   $select_prev_pass->execute([$user_id]);
+   $fetch_prev_pass = $select_prev_pass->fetch(PDO::FETCH_ASSOC);
+   $prev_pass = $fetch_prev_pass['password'];
+   $old_pass = sha1($_POST['old_pass']);
+   $old_pass = filter_var($old_pass, FILTER_SANITIZE_STRING);
+   $new_pass = sha1($_POST['new_pass']);
+   $new_pass = filter_var($new_pass, FILTER_SANITIZE_STRING);
+   $confirm_pass = sha1($_POST['confirm_pass']);
+   $confirm_pass = filter_var($confirm_pass, FILTER_SANITIZE_STRING);
+
+   if($old_pass != $empty_pass){
+      if($old_pass != $prev_pass){
+         $message[] = 'old password not matched!';
+      }elseif($new_pass != $confirm_pass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         if($new_pass != $empty_pass){
+            $update_pass = $conn->prepare("UPDATE `users` SET password = ? WHERE id = ?");
+            $update_pass->execute([$confirm_pass, $user_id]);
+            $message[] = 'password updated successfully!';
+         }else{
+            $message[] = 'please enter a new password!';
+         }
+      }
+   }  
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>update profile</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="loader-container">
+   <div class="loader">
+   <div>
+      <ul>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+         <li>
+         <svg fill="currentColor" viewBox="0 0 90 120">
+            <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+         </svg>
+         </li>
+      </ul>
+   </div><span>Loading..</span></div>
+   <div class="blur-background"></div>
+</div>
+   
+<!-- header section starts  -->
+<header class="header">
+
+   <section class="flex">
+
+      <div class="icons">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+               $count_user_comments = $conn->prepare("SELECT * FROM `comments` WHERE user_id = ?");
+               $count_user_comments->execute([$user_id]);
+               $total_user_comments = $count_user_comments->rowCount();
+               $count_user_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ?");
+               $count_user_likes->execute([$user_id]);
+               $total_user_likes = $count_user_likes->rowCount();
+         ?>
+         <a href="user_comments.php" style="color: #a6bcce; font-size: 2.5rem; margin-right: 1.0rem;"><i class="fas fa-comment"></i><span style="margin: 0 0.40rem; color: #a6bcce;">(<?= $total_user_comments; ?>)</span></a>
+         <a href="user_likes.php" style="color: #a6bcce; font-size: 2.5rem;"><i class="fas fa-heart"></i><span style="margin: 0 0.40rem; color: #a6bcce;">(<?= $total_user_likes; ?>)</span></a>
+      </div>
+
+      <form action="search.php" method="POST" class="search-form">
+         <input type="text" name="search_box" class="box" maxlength="100" placeholder="search for books" required>
+         <button type="submit" class="fas fa-search" name="search_btn"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars" style="margin-left: 1.2rem;"></div>
+         <div id="search-btn" class="fas fa-search" style="margin-left: 1.2rem;"></div>
+         <div id="user-btn" class="fas fa-user" style="margin-left: 1.2rem;"></div>
+      </div>
+
+      <nav class="navbar">
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="posts.php"> <i class="fas fa-angle-right"></i> posts</a>
+         <a href="all_category.php"> <i class="fas fa-angle-right"></i> category</a>
+         <a href="added_blogs.php"> <i class="fas fa-angle-right"></i> blogs</a>
+         <a href="added_policies.php"> <i class="fas fa-angle-right"></i> policies</a>
+         
+      </nav>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <p class="name">Hello! <font color="#4834d4"><?= $fetch_profile['name']; ?></font></p>
+         <a href="update.php" class="btn">update profile</a>
+         <!-- THE DEVELOPER HIDE THIS FEATURES FOR MEANTIME
+            <div class="flex-btn">
+               <a href="login.php" class="option-btn">login</a>
+               <a href="register.php" class="option-btn">register</a>
+            </div> 
+         ENDS HERE! -->
+         <a href="./login.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         <?php
+            }else{
+         ?>
+            <p class="name">please login first!</p>
+            <a href="login.php" class="option-btn">login</a>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+
+</header>
+
+<div class="box-container">
+   <?php
+      }else{
+   ?>
+      <p class="name">login or register!</p>
+      <div class="flex-btn">
+         <a href="login.php" class="option-btn">login</a>
+         <a href="register.php" class="option-btn">register</a>
+      </div> 
+   <?php
+      }
+   ?>
+
+      <!-- THE DEVELOPER HIDE THIS FEATURES FOR MEANTIME
+         <div class="box">
+            <p>authors</p>
+            <div class="flex-box">
+            <?php
+               /*
+               $select_authors = $conn->prepare("SELECT DISTINCT name FROM `admin` LIMIT 10");
+               $select_authors->execute();
+               if($select_authors->rowCount() > 0){
+                  while($fetch_authors = $select_authors->fetch(PDO::FETCH_ASSOC)){ 
+               */
+            ?>
+               /*
+               <a href="author_posts.php?author= <?= $fetch_authors['name']; ?>" class="links"><?= $fetch_authors['name']; ?></a>
+               */
+               <?php
+            /*} 
+            }else{
+               echo '<p class="empty">no posts added yet!</p>';
+            } */
+            ?>  
+            <a href="authors.php" class="btn">view all</a> 
+            </div>
+         </div>
+      ENDS HERE! -->
+   
+
+</div>
+<!-- header section ends -->
+
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>update profile</h3>
+      <input type="text" name="name" placeholder="<?= $fetch_profile['name']; ?>" class="box" maxlength="50">
+      <input type="email" name="email" placeholder="<?= $fetch_profile['email']; ?>" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="old_pass" placeholder="enter your old password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="new_pass" placeholder="enter your new password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="confirm_pass" placeholder="confirm your new password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="update now" name="submit" class="btn">
+   </form>
+
+</section>
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+   // Select the loader and background elements
+   const loaderContainer = document.querySelector('.loader-container');
+   const blurBackground = document.querySelector('.blur-background');
+
+   // Show the loader and background on page load
+   window.addEventListener('load', () => {
+   loaderContainer.style.display = 'flex';
+   blurBackground.style.display = 'block';
+   });
+
+   // Hide the loader and background after 3 seconds
+   setTimeout(() => {
+   loaderContainer.style.display = 'none';
+   blurBackground.style.display = 'none';
+   }, 3000);
+
+</script>
+
+</body>
+</html>
